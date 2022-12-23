476bca398d72a62b3261832d22d1da896eecc7ea
Mukil <mukil.george@icloud.com>
2022-12-23 23:52:58 +0100

    Cration UO

---
+++ creation OU.ps1
@@ -0,0 +1,34 @@
+
+Import-Module ActiveDirectory
+
+function Creation-UO {
+  param(
+    [string]$NomUO,
+    [string]$ParentUO
+  )
+
+  if (Get-ADOrganizationalUnit -Filter "Name -eq '$NomUO'" -ErrorAction SilentlyContinue) {
+    Write-Output "l'unité d’organisation'$NomUO' existe déjà."
+    return
+  }
+
+  try {
+    New-ADOrganizationalUnit -Name $NomUO -Path $ParentUO
+    Write-Output "l'unité d’organisation'$NomUO' a été créé avec succès."
+  } catch {
+    Write-Output "Erreur lors de la création de l'unité d’organisation '$NomUO': $($Error[0])"
+  }
+
+  $createChildOUs = Read-Host "Voulez-vous créer des sous unités d’organisations dans  '$NomUO'? (O/N)"
+
+  while ($createChildOUs -eq "O") {
+    $childNomUO = Read-Host "Entrez le nom de le sous unité d’organisation à créer:"
+    Creation-UO -NomUO $childNomUO -ParentUO "OU=$NomUO,$ParentUO"
+    $createChildOUs = Read-Host "Voulez-vous créer des sous OU dans OU '$NomUO'? (O/N)"
+  }
+}
+
+
+$mainNomUO = Read-Host "Entrez le nom de l'OU principal à créer:"
+
+Creation-UO -NomUO $mainNomUO -ParentUO "DC=mk,DC=lan"
