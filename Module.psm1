a3035c1164dc068a0365db2b19fba34c05b90140
Theo PAYEN <tt.payen@gmail.com>
2022-12-24 12:16:50 +0100

    rm test

--- Module.psm1
+++ Module.psm1
@@ -56,14 +56,3 @@ function Write-Success {
     )
     Write-Logs -Type "Success" -Message $Message -Commentaire $Commentaire -FilePath $FilePath
 }
-
-# 2 possibilité sois je met le type derreur directement dans la fonction sois je met sans en sachant que si  je met les erreur directement
-# a toi de choisir tu prefaire quoi 
-# ducoup j'ai viré la class trop relou ducoup on va garder le fichier psm1
-
-Write-Error -Message "Error message" -Commentaire "Commentaire" -FilePath "logs.json"
-Write-Info -Message "Info message" -Commentaire "Commentaire" -FilePath "logs.json"
-Write-Success -Message "Success message" -Commentaire "Commentaire" -FilePath "logs.json"
-Write-Warning -Message "Warning message" -Commentaire "Commentaire" -FilePath "logs.json"
-
-Write-Logs -Type "Error" -Message "Error message" -Commentaire "Commentaire" -FilePath "logs.json"
--- test.ps1
+++
@@ -1,8 +0,0 @@
-# Demande de saisie d'un mot de passe
-$motDePasse = Read-Host -AsSecureString "Entrez votre mot de passe :"
-
-# Conversion du mot de passe en chaîne de caractères
-$motDePasseChaine = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($motDePasse))
-
-# Affichage du mot de passe
-Write-Output "Votre mot de passe est : $motDePasseChaine"
