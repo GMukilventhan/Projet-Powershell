2b79fcfd92d682e64674f91f15b10d6faf841244
Theo PAYEN <tt.payen@gmail.com>
2022-12-29 19:08:09 +0100

    Update OUs.ps1

--- OUs.ps1
+++ OUs.ps1
@@ -5,14 +5,13 @@
 # Les unités d'organisation parent BIODEVOPS et enfant ETUDIANTS sont crées
 
 # Import du fichier CSV
-$CSVpath = "C:\ScriptsBiodevops\biodevops_ou.csv"
+$CSVpath = "ou_csv_NOM_A_MODIFIER.csv"
 
 
 # Boucle Foreach pour parcourir le fichier CSV
 
 
-
-function new-OufromCsv {
+function New-OufromCsv {
     param (
         $CSVpath
     )
@@ -21,14 +20,30 @@ function new-OufromCsv {
     Foreach($OU in $CSVdata){
         $OUname = $OU.name
         $OUpath = $OU.path
-        $testOu = Get-ADOrganizationalUnit -LDAPFilter "(name=$(OU.name))" | Format-Table Name
-        
-        if {$testOu}
-        try {
-            New-ADOrganizationalUnit -Name $OUname -Path $OUpath
-        }catch {
-            Write-Host "une erreur est survenu"
-            Write-Host $_.Exception.GetType()
+        $testOu = Get-ADOrganizationalUnit -LDAPFilter "(name=$(OU.name))"
+        if ($null -eq $testOu){
+            Write-Output 'coucou'
+        }else {
+                        try {
+                New-ADOrganizationalUnit -Name $OUname -Path $OUpath
+            }catch {
+                Write-Host "une erreur est survenu"
+                Write-Host $_.Exception.GetType()
+            }
         }
     }
 }
+
+New-OufromCsv -CSVpath $CSVpath
+
+<#
+
+
+$ous = Get-ADOrganizationalUnit -Filter * | Sort-Object Name
+
+foreach ($ou in $ous)
+{
+    $groupName = "Distribution Group - $($ou.Name)"
+    New-ADGroup -Name $groupName -Type Distribution -Path $ou.DistinguishedName
+    Write-Host "Group $groupName created in OU $($ou.Name)"
+}#>
