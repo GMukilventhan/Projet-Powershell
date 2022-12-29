43d6d80b3a87d07e31bd947714607ffe98810e8e
Theo PAYEN <tt.payen@gmail.com>
2022-12-29 18:56:25 +0100

    update

--- OUs.ps1
+++ OUs.ps1
@@ -6,14 +6,29 @@
 
 # Import du fichier CSV
 $CSVpath = "C:\ScriptsBiodevops\biodevops_ou.csv"
-$CSVdata = Import-CSV -Path $CSVpath -Delimiter ";" -Encoding Default
+
 
 # Boucle Foreach pour parcourir le fichier CSV
-Foreach($OU in $CSVdata){
 
-$OUname = $OU.name
-$OUpath = $OU.path
 
-New-ADOrganizationalUnit -Name $OUname -Path $OUpath
 
+function new-OufromCsv {
+    param (
+        $CSVpath
+    )
+
+    $CSVdata = Import-CSV -Path $CSVpath -Delimiter ";" -Encoding Default
+    Foreach($OU in $CSVdata){
+        $OUname = $OU.name
+        $OUpath = $OU.path
+        $testOu = Get-ADOrganizationalUnit -LDAPFilter "(name=$(OU.name))" | Format-Table Name
+        
+        if {$testOu}
+        try {
+            New-ADOrganizationalUnit -Name $OUname -Path $OUpath
+        }catch {
+            Write-Host "une erreur est survenu"
+            Write-Host $_.Exception.GetType()
+        }
+    }
 }
