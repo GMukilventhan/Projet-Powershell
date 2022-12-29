846a54613c6453f98cdab59f99c41149dd155f7b
MaximeHab <104725706+MaximeHab@users.noreply.github.com>
2022-12-29 16:38:17 +0100

    Add files via upload

---
+++ OUs.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module ActiveDirectory
+
+# Pré-requis script :
+# Le fichier CSV dans le chemin indiqué
+# Les unités d'organisation parent BIODEVOPS et enfant ETUDIANTS sont crées
+
+# Import du fichier CSV
+$CSVpath = "C:\ScriptsBiodevops\biodevops_ou.csv"
+$CSVdata = Import-CSV -Path $CSVpath -Delimiter ";" -Encoding Default
+
+# Boucle Foreach pour parcourir le fichier CSV
+Foreach($OU in $CSVdata){
+
+$OUname = $OU.name
+$OUpath = $OU.path
+
+New-ADOrganizationalUnit -Name $OUname -Path $OUpath
+
+}
