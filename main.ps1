7b43f3fbc2f7888f374f8cff5e0b052d5b9fe5fa
Theo PAYEN <tt.payen@gmail.com>
2022-12-29 23:14:28 +0100

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -2,3 +2,5 @@ $ErrorActionPreference = "Stop"
 Import-Module $PSScriptRoot/Module.psm1
 global $filelogs = "logs.json"
 Write-Success -Message "pas d'erreur" -Commentaire "mon super commentaire" -FilePath $filelogs
+test-OUexist -OUname "test"
+$?
