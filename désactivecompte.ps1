dba002d76f3bd568a56f2aa0614d6caf527805b2
mohamed wazane <92384537+mowazane@users.noreply.github.com>
2022-12-31 00:19:27 +0100

    désactive compte
    en cours de travaux

---
+++ désactivecompte.ps1
@@ -0,0 +1,11 @@
+﻿
+$users = Import-Csv -Path "C:\users.csv"
+
+foreach ($user in $users) {
+  # Récupére l'objet utilisateur correspondant dans Active Directory
+  $adUser = Get-ADUser -Identity $user.Username -ErrorAction SilentlyContinue
+  if ($adUser) {
+    # Désactive l'utilisateur s'il existe dans Active Directory
+    Disable-ADAccount -Identity $adUser
+  }
+}
