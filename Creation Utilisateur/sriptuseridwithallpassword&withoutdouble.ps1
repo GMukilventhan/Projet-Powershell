1fd23acc529c0a36d94b88e36a98f7ecd0a56cc3
Theo PAYEN <tt.payen@gmail.com>
2023-01-02 20:10:32 +0100

    Update sriptuseridwithallpassword&withoutdouble.ps1

--- Creation Utilisateur/sriptuseridwithallpassword&withoutdouble.ps1
+++ Creation Utilisateur/sriptuseridwithallpassword&withoutdouble.ps1
@@ -22,74 +22,76 @@ foreach ($User in $CSVdata) {
     $UserAnneeEtude = $User.AnneeEtude
     $UserFiliere = $User.Filiere
     $UserPromotion = $User.Promotion
-   $UserClasse = $UserAnnee + "_" + $UserPromotion
-   $UserPath = "OU=$UserClasse,OU=$UserFiliere,OU=$UserAnneeEtude,OU=$UserAnnee,OU=ETUDIANTS,OU=BIODEVOPS,DC=mk,DC=lan"
-   $GroupNameSecurity =  "Grp_Securite_" + $UserAnnee + "_" + $UserPromotion
+    $UserClasse = $UserAnnee + "_" + $UserPromotion
+    $UserPath = "OU=$UserClasse,OU=$UserFiliere,OU=$UserAnneeEtude,OU=$UserAnnee,OU=ETUDIANTS,OU=BIODEVOPS,DC=mk,DC=lan"
+    $GroupNameSecurity =  "Grp_Securite_" + $UserAnnee + "_" + $UserPromotion
     $GroupNameDistribution = "Grp_Distribution_" + $UserAnnee + "_" + $UserPromotion
     $GroupDistributionEmail = $UserPromotion + "." + $UserAnnee + "@biodevops.eu"
     $GroupPath = "OU=$UserClasse,OU=$UserFiliere,OU=$UserAnneeEtude,OU=$UserAnnee,OU=ETUDIANTS,OU=BIODEVOPS,DC=mk,DC=lan"
     $currentYear = (Get-Date).Year
     $uniqueRandomNumbers = @()
-for ($i = 0; $i -lt 10; $i++) {
-  $uniqueRandomNumbers += Get-Random -Minimum 0 -Maximum 9
-}
-$uniqueId = "U$currentYear" + (-join $uniqueRandomNumbers)
 
-   # Vérifie si le groupe de sécurité et distribution  existe déjà
-$GroupExists = Get-ADGroup -Filter {Name -eq $GroupNameSecurity} -ErrorAction SilentlyContinue
-$GroupExists = Get-ADGroup -Filter {Name -eq $GroupNameDistribution} -ErrorAction SilentlyContinue
-
-# Si le groupe de sécurité n'existe pas, le créer
-if (!$GroupExists) {
-    New-ADGroup -Name $GroupNameSecurity -Path $UserPath -GroupScope Global -GroupCategory Security
-   Write-Output "Création du groupe de securite : $GroupNameSecurity !"
-    New-ADGroup -Name $GroupNameDistribution -Path $UserPath -GroupScope Global -GroupCategory Distribution -OtherAttributes @{'mail'= $GroupDistributionEmail}
-    Write-Output "Création du groupe de distribution : $GroupNameDistribution !"
-}
-
-$UserExists = Get-ADUser -Filter {SamAccountName -eq $uniqueId} -ErrorAction SilentlyContinue
-
-# Si l'utilisateur existe déjà
-if ($UserExists) {
-    # Génère un nouveau nom d'utilisateur en ajoutant un numéro au nom de l'utilisateur
-    $i = 1
-    while ($UserExists) {
-        $UserLogon = $UserFirstname.ToLower() + "." + $UserLastname.ToLower() + $i
-        $UPN = "$UserLogon@biodevops.local"
-        $UserEmail = "$UserLogon@biodevops.eu"
-        $UserExists = Get-ADUser -Filter {SamAccountName -eq $uniqueId} -ErrorAction SilentlyContinue
-        $i++
-        $UserDisplay = "$UserFirstname $UserLastname $i"
+    for ($i = 0; $i -lt 10; $i++) {
+        $uniqueRandomNumbers += Get-Random -Minimum 0 -Maximum 9
     }
-}
+    $uniqueId = "U$currentYear" + (-join $uniqueRandomNumbers)
+
+    # Vérifie si le groupe de sécurité et distribution  existe déjà
+    $GroupExists = Get-ADGroup -Filter {Name -eq $GroupNameSecurity} -ErrorAction SilentlyContinue
+    $GroupExists = Get-ADGroup -Filter {Name -eq $GroupNameDistribution} -ErrorAction SilentlyContinue
+
+    # Si le groupe de sécurité n'existe pas, le créer
+    if (!$GroupExists) {
+        New-ADGroup -Name $GroupNameSecurity -Path $UserPath -GroupScope Global -GroupCategory Security
+        Write-Output "Création du groupe de securite : $GroupNameSecurity !"
+        New-ADGroup -Name $GroupNameDistribution -Path $UserPath -GroupScope Global -GroupCategory Distribution -OtherAttributes @{'mail'= $GroupDistributionEmail}
+        Write-Output "Création du groupe de distribution : $GroupNameDistribution !"
+    }
+
+    $UserExists = Get-ADUser -Filter {SamAccountName -eq $uniqueId} -ErrorAction SilentlyContinue
+
+    # Si l'utilisateur existe déjà
+    if ($UserExists) {
+        # Génère un nouveau nom d'utilisateur en ajoutant un numéro au nom de l'utilisateur
+        $i = 1
+        while ($UserExists) {
+            $UserLogon = $UserFirstname.ToLower() + "." + $UserLastname.ToLower() + $i
+            $UPN = "$UserLogon@biodevops.local"
+            $UserEmail = "$UserLogon@biodevops.eu"
+            $UserExists = Get-ADUser -Filter {SamAccountName -eq $uniqueId} -ErrorAction SilentlyContinue
+            $i++
+            $UserDisplay = "$UserFirstname $UserLastname $i"
+        }
+    }
 
 
-   # Si l'utilisateur n'existe pas, le créer
-   else {
-       New-ADUser -Name $UserDisplay `
-                  -GivenName $UserFirstname `
-                  -Surname $UserLastname `
-                  -DisplayName $UserDisplay `
-                  -SamAccountName $UserLogon `
-                  -UserPrincipalName $UPN `
-                  -EmailAddress $UserEmail `
-                  -Title $UserTitle `
-                  -Department $UserClasse `
-                  -Company $UserCpny `
-                  -Path $UserPath `
-                  -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) `
-                   -ChangePasswordAtLogon $True `
-                   -Enabled $true
-       Write-Output "Création de l'utilisateur : $UserDisplay !"
-       Add-ADGroupMember -Identity $GroupNameSecurity -Members $UserLogon
-       Add-ADGroupMember -Identity $GroupNameDistribution -Members $GroupNameSecurity
+        # Si l'utilisateur n'existe pas, le créer
+        else {
+            New-ADUser `
+                -Name $UserDisplay `
+                -GivenName $UserFirstname `
+                -Surname $UserLastname `
+                -DisplayName $UserDisplay `
+                -SamAccountName $UserLogon `
+                -UserPrincipalName $UPN `
+                -EmailAddress $UserEmail `
+                -Title $UserTitle `
+                -Department $UserClasse `
+                -Company $UserCpny `
+                -Path $UserPath `
+                -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) `
+                -ChangePasswordAtLogon $True `
+                -Enabled $true
 
-     
-   }
-$users = @()
-$users += New-Object -TypeName PSObject -Property @{
-  "Username" = $UserLogon
-  "Password" = $password
-}
-$users | Export-Csv -Path "C:\password.csv" -Append -NoType
+            Write-Output "Création de l'utilisateur : $UserDisplay !"
+            Add-ADGroupMember -Identity $GroupNameSecurity -Members $UserLogon
+            Add-ADGroupMember -Identity $GroupNameDistribution -Members $GroupNameSecurity
+        }
+
+    $users = @()
+    $users += New-Object -TypeName PSObject -Property @{
+        "Username" = $UserLogon
+        "Password" = $password
+    }
+    $users | Export-Csv -Path "C:\password.csv" -Append -NoType
 }
