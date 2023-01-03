acfddfa7345ed2acffa9cbe1ba20290b5dcfddc7
Theo PAYEN <tt.payen@gmail.com>
2023-01-03 10:37:00 +0100

    update

--- test.ps1
+++ test.ps1
@@ -1,6 +1,8 @@
 $uniqueRandomNumbers = @()
 
-for ($i = 0; $i -lt 10; $i++) {
-    $uniqueRandomNumbers += Get-Random -Minimum 0 -Maximum 9
-}
-Get-Random -Count 3 
+
+$uniqueRandomNumbers = Get-Random -count 10 -Minimum 0 -Maximum 9
+
+#Get-Random -Count 3
+
+$uniqueRandomNumbers
