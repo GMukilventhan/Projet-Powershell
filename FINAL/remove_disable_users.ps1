2d637976a5ce03df70d2cf40b2bec866a435fbc1
mohamed wazane <92384537+mowazane@users.noreply.github.com>
2023-01-04 14:26:07 +0100

    update

--- FINAL/remove_disable_users.ps1
+++ FINAL/remove_disable_users.ps1
@@ -5,14 +5,23 @@ Import-Module $PSScriptRoot/Module.psm1
 $global:filelogs = "Logs/remove.json"
 
 $users = Get-ADUser -Filter *
-#TODO LOGS
+
 foreach ($user in $users) {
     if ($user.Enabled -eq $false) {
         $disableDate = $user.AccountDisabledDate
         $timeSinceDisable = New-TimeSpan -Start $disableDate -End (Get-Date)
-    if ($timeSinceDisable.Days -gt 30) {
-        Remove-ADUser -Identity $user
-        Write-Success -Message "l'utilisateur a été supprimé:" -Commentaire $user
-    }
+        if ($timeSinceDisable.Days -gt 30) {
+            Remove-ADUser -Identity $user
+            Write-Success -Message "l'utilisateur a été supprimé:" -Commentaire $user
+        }
+    }else{
+        $inactiveDays = 60
+        $time = (Get-Date).Adddays(-($inactiveDays))
+        if ($user.LastLogonTimeStamp -lt $time){
+            Disable-ADAccount -Identity $user
+        }
     }
 }
+
+
+
